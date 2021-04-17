c5aa4fcec1435e84da9c615cbe968c27d567b7bc
jairo guzman <guzman_j@qwasar.io>
2021-04-17 21:50:54 +0000

    need to merge

--- my_blockchain.c
+++ my_blockchain.c
@@ -92,7 +92,6 @@ int add_node(int argc, char** argv, node* head, sync_status* status)
 
 int ls_l(int argc, char** argv, node* head, sync_status* status)
 {
-    head = head->next;
     while(head != NULL)
     {
         if (!argv[1])
@@ -103,6 +102,7 @@ int ls_l(int argc, char** argv, node* head, sync_status* status)
         {
             while (head->blocks != NULL)
             {
+                printf("LOCKITO\n");
                 printf("%s", head->blocks->bid);
                 head->blocks = head->blocks->next; 
             }
